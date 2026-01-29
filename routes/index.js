7d69a3e0a6f524408ca6788bca9fa638f946e1bc
alanbabu3774 <157644521+alanbabu3774@users.noreply.github.com>
2026-01-29 15:28:07 -0500

    DB Connection Successfull

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,29 @@
 var express = require('express');
 const { timeStamp } = require('node:console');
 var router = express.Router();
+const db = require('../config/database');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.json({
-     title: "Express",
-     status: "Live", 
-     timestamp: new Date()
+/* GET db test route */
+router.get('/db-test', async function(req, res, next) {
+  try {
+    const [rows] = await db.query('SELECT 1 + 1 AS result');
+    res.json({
+        status: 'Connected',
+        math_check: rows[0].result
     });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
-
-module.exports = router;
+/* GET test_table data */
+router.get('/my-data', async function(req, res, next) {
+  try {
+    const [rows] = await db.query('SELECT * FROM test_table');    
+    res.json({
+        status: 'Success',
+        data: rows
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
